409019d5ba6b1e983c632ec119cb202016a8afd2
Kaushik <kaushikar2211@gmail.com>
2026-05-22 14:27:56 -0600

    push

--- api/progress.js
+++ api/progress.js
@@ -1,6 +1,11 @@
 import { Redis } from "@upstash/redis";
+import { config } from "dotenv";
+config({ path: ".env.development.local" });
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url:   process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
+});
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
